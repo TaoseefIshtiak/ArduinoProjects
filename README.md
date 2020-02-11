fec0ee3f3eee483ffe7e2c813cdd525467119c7f
TaoseefIshtiak <taoseef.ishtiak@northsouth.edu>
2020-02-11 12:29:07 +0600

    basic arduino led code uploaded

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ArduinoProjects
+This repository contains the code of the arduino projects that I worked during my BS degree completion. The codes are free to use and please let me know @taoseef.ishtiak@northsouth.edu, if you find any issue
---
+++ _1_led/_1_led.ino
@@ -0,0 +1,15 @@
+/*decalring variables (global)*/
+int led=10;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(led, HIGH);
+delay(1000);
+digitalWrite(led, LOW);
+delay(500);
+}
